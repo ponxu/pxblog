413ddfdaee27a4b5298ac3ea38f1d24c92eab4f7
ponxu <xwz689@gmail.com>
2012-12-21 21:43:25 +0800

    aa

--- model.py
+++ model.py
@@ -6,9 +6,42 @@ mdb = sdb = database.Connection(db_host, db_name, db_user, db_passwd, max_idle_t
 if is_sae: sdb = database.Connection(db_host2, db_name2, db_user2, db_passwd2, max_idle_time)
 
 class _Post:
-    def query(self):
-        sql = 'select * from px_post'
-        return None
+    def query(self, page, page_size, type='post', keywords=None, tagid=None, status=None):
+        ''' 综合查询 '''
+        sql = "select * from px_post"
+        count_sql = "select count(*) from px_post"
+
+        condition = ""
+        if type:
+            condition += " and type='%s'" % type
+        if status:
+            if not isinstance(status, list):
+                status = [status]
+            condition += " and status in ('%s')" % "','".join(status)
+        if keywords:
+            like = ""
+            for kw in keywords.split(' '):
+                if kw:
+                    like += " or title like '%%%%%s%%%%'" % kw
+                    like += " or content like '%%%%%s%%%%'" % kw
+            if like:
+                condition += " and (" + like[4:] + ")"
+        if tagid:
+            if not isinstance(tagid, list):
+                tagid = [tagid]
+            condition += (" and exists (select tag_id from px_post_tag pt where px_post.id=pt.post_id and tag_id in (%s))"
+                          % ",".join([str(d) for d in tagid]))
+
+        if condition:
+            sql += ' where ' + condition[5:]
+            count_sql += ' where ' + condition[5:]
+
+        sql += ' order by top desc, id asc limit %d,%d' % ((page - 1) * page_size, page_size)
+
+        print sql
+        print count_sql
+
+        return sdb.query(sql), sdb.get(count_sql)['count(*)']
 
     def add(self, post):
         pass
@@ -22,6 +55,8 @@ def create_init_table():
 
 # Test
 if __name__ == "__main__":
-    print mdb
-    print sdb
-    print Post
+    tagid = 1
+    type='page'
+    keywords='1 2 3 4'
+    status= 'password'
+    print Post.query(1, 100, type=type, keywords=keywords, tagid=tagid, status=status)
