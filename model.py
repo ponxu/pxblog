b5a319fee227e55665e167a314e1650a2129cab0
ponxu <xwz689@gmail.com>
2012-12-21 19:41:06 +0800

    aa

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ if is_sae: sdb = database.Connection(db_host2, db_name2, db_user2, db_passwd2, m
 
 class _Post:
     def query(self):
+        sql = 'select * from px_post'
         return None
 
     def add(self, post):
